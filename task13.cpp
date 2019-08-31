6f0a8044b4eb408fdb3bf01257d268385402a448
sumibhattacharjee <54468020+sumibhattacharjee@users.noreply.github.com>
2019-08-31 14:49:05 +0530

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int a[60],i,n,large,small;
+	printf("how many elements:");
+	scanf("%d",&n);
+	printf("enter the array:");
+	for(i=0;i<n;++i)
+	scanf("%d",&a[i]);
+	large=small=a[0];
+	for(i=1;i<n;++i)
+	{
+		if(a[i]>large)
+		large=a[i];
+		if(a[i]<small)
+		small=a[i];
+	}
+	printf(" the largest element is:%d",large);
+	printf("\nthe smallest element is :%d",small);
+}
+
