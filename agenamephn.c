1416a732c892bd37288ef619c3b6ba06937746e5
sumibhattacharjee <54468020+sumibhattacharjee@users.noreply.github.com>
2019-08-31 13:57:01 +0530

    Add files via upload

---
+++ agenamephn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>;
+main()
+{
+	printf(" enter your name :");
+	char name[30];
+	scanf("%s",&name);
+	printf("your name is :%s\n",name);
+	printf("enter your age");
+	int age=0;
+	scanf("%d",&age);
+	printf("your age is:%d\n",age);
+	printf("enter your phone number:");
+	int phne=0;
+	scanf("%d",&phne);
+	printf("your phone number is:%d",phne);
+	
+}
